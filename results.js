eb36d3041043d33c56fc0184e299ad17523ef100
Dassah-Felix <dassahfelix12@gmailcom>
2022-09-01 14:31:54 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,13 @@
+const results=[{ name: "Abdul Hafiz", class: "form three", grade: "8"},
+{ name: "Abdul Hafiz", class: "form three", grade: "8"},
+{ name: "NAEM", class: "form three", grade: "7"},
+{ name: "Sadika", class: "form three", grade: "3"},
+{ name: "Fortunate", class: "form three", grade: "9"},
+]
+
+
+
+
+
+
+module.exports=results
